1c0fcfbc8d31dec4027436d11b4a2896d073521d
MahtHdz <maht_hernandez@outlook.com>
2022-06-15 16:28:40 -0500

    Data extrator in basic mode finished

--- src/utils/excelDataExtractor.js
+++ src/utils/excelDataExtractor.js
@@ -41,7 +41,12 @@ const getDataFromOneSheet = async (filepath, sheetName, fields) => {
         save the data from current cell, if not,
         find the field title in the current cell.
       */
-      if (fieldsFlag[count] && cell.value !== null) clientData[currentField].push(cell.value)
+      if (fieldsFlag[count] && cell.value !== null) {
+        if (cell.value instanceof Object)
+          clientData[currentField].push(cell.value.result)
+        else
+          clientData[currentField].push(cell.value)
+      }
       else fieldsName.forEach((name) => {
         // Finding the field title in the current cell
         if (cell.value === name) {
@@ -58,18 +63,18 @@ const getDataFromOneSheet = async (filepath, sheetName, fields) => {
 }
 
 const getDataFromAllSheets = async (filepath, sheetsFields) => {
-  let clientData = {}
+  let sheetsFieldsData = []
   //let worksheets = []
   let sheetsFieldsFlag = []
   let sheetsData = []
   const workbook = new ExcelJS.Workbook()
   // Getting the layout headers
-  const sheetsFieldsName = Object.values(sheetsFields)
+  const sheetsFieldsName = Object.entries(sheetsFields)
   // Initializing the fields flag array
-  sheetsFieldsName.forEach((sheet, index) => {
+  sheetsFieldsName.forEach(sheet => {
     let fieldsFlag = []
-    sheet.forEach((field, index) => { fieldsFlag[index] = false })
-    sheetsFieldsFlag[index] = fieldsFlag
+    sheet[1].forEach(() => { fieldsFlag.push(false) })
+    sheetsFieldsFlag.push(fieldsFlag)
   })
   // Trying to get the workbook from excel file
   try { await workbook.xlsx.readFile(filepath) }
@@ -84,8 +89,47 @@ const getDataFromAllSheets = async (filepath, sheetsFields) => {
     sheetsData.push([sheet.name, fieldsArray])
     //worksheets.push(sheet)
   })
-  console.log(sheetsData)
-  // return clientData
+  sheetsData.forEach(sheet => {
+    let fieldsData = {}
+    sheetsFieldsName.forEach((sheetFields, index) => {
+      if (sheet[0] === sheetFields[0]) {
+        // Counter to verify if all the fields are found
+        let count = 0
+        sheet[1].forEach((column) => {
+          let currentField = ""
+          // If count is less than the length of the fieldsFlags, then there's still data to be saved
+          if (count === sheetsFieldsFlag[index].length) return
+          // Getting the data from the current column
+          column.eachCell((cell, rowNumber) => {
+            /*
+              If the field title was found and still there
+              are data to be saved in the current column,
+              save the data from current cell, if not,
+              find the field title in the current cell.
+            */
+            if (sheetsFieldsFlag[index][count] && cell.value !== null) {
+              if (cell.value instanceof Object)
+                fieldsData[currentField].push(cell.value.result)
+              else
+                fieldsData[currentField].push(cell.value)
+            }
+            else sheetFields[1].forEach((name) => {
+              // Finding the field title in the current cell
+              if (cell.value === name) {
+                sheetsFieldsFlag[index][count] = true
+                fieldsData[name] = []
+                currentField = name
+                return
+              }
+            })
+          })
+          if(sheetsFieldsFlag[index][count]) count++
+        })
+      }
+    })
+    sheetsFieldsData.push({ sheetName:sheet[0], fieldsData })
+  })
+  return sheetsFieldsData
 }
 
 const excelDataExtractor = {
